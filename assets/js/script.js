4edf2937034bb82c9aa4d180845b055ee8c69d53
Diego Castan Lopes <46874383850>
2023-05-29 17:01:04 -0300

    main

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,7 @@ window.onload = function() {
                         `</div>`+
                     `</div>`+
                     `<div id="shop">`+
-                        `<a onclick="AddCarrinho('${produto.produto}', document.getElementById('num').value, '${parseFloat(produto.preco.replace("U$ ", ""))}', ${pos})" href="#"><img src="../assets/imgs/cart-shopping-solid.svg" alt="Carrinho"></a>`+
+                        `<a data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="AddCarrinho('${produto.produto}', document.getElementById('num').value, '${parseFloat(produto.preco.replace("U$ ", ""))}', '${produto.img}', ${pos})" href="#"><img src="../assets/imgs/cart-shopping-solid.svg" alt="Carrinho"></a>`+
                     `</div>`+
                 `</article>`+
             `</section>`
@@ -106,7 +106,7 @@ let produtos = [
 var pos = 1
 
 
-function AddCarrinho(produto, qtd, valor, posicao)
+function AddCarrinho(produto, qtd, valor, img, posicao)
 	{
         for(i=1; i<=99; i++) {
             var prod = localStorage.getItem("produto" + i + "");
@@ -116,32 +116,9 @@ function AddCarrinho(produto, qtd, valor, posicao)
         }
         posicao = posicao + 1
         console.log(pos)
+        localStorage.setItem("img" + posicao, img)
 		localStorage.setItem("produto" + posicao, produto);
 		localStorage.setItem("qtd" + posicao, qtd);
 		valor = valor * qtd;
 		localStorage.setItem("valor" + posicao, valor);
-		alert("Produto adicionado ao carrinho!");
 	}
-
-    var total = 0; // variável que retorna o total dos produtos que estão na LocalStorage.
-    var i = 0;     // variável que irá percorrer as posições
-    var valor = 0; // variável que irá receber o preço do produto convertido em Float.
-    
-    for(i=1; i<=99; i++) // verifica até 99 produtos registrados na localStorage
-    {
-        var prod = localStorage.getItem("produto" + i + ""); // verifica se há recheio nesta posição. 
-        if(prod != null) 
-        {	
-            // exibe os dados da lista dentro da div itens
-            document.getElementById("itens").innerHTML += localStorage.getItem("qtd" + i) + " x ";
-            document.getElementById("itens").innerHTML += localStorage.getItem("produto" + i);
-            document.getElementById("itens").innerHTML += " ";
-            document.getElementById("itens").innerHTML += "R$: " + localStorage.getItem("valor" + i) + "<hr>";
-            
-            // calcula o total dos recheios
-            valor = parseFloat(localStorage.getItem("valor" + i)); // valor convertido com o parseFloat()
-            total = (total + valor); // arredonda para 2 casas decimais com o .toFixed(2)
-        }
-    } 
-    // exibe o total dos recheios
-    document.getElementById("total").innerHTML = total.toFixed(2);
